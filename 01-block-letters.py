d5f588ea87a82b1bdc4392183ad04058373aeb73
Benett Viszokai <benettviszokai@gmail.com>
2025-12-02 08:00:16 +0100

    Created 01-block-letters.py

---
+++ 01-block-letters.py
@@ -0,0 +1,20 @@
+"""Block Letters
+
+In this project, the task was to create ASCII art using Python. The goal was to
+print the initials of my name in large, block-style characters by arranging
+keyboard symbols line by line. This exercise introduces the idea of representing
+shapes with text and helps practice basic print statement usage.
+"""
+
+# Benett Viszokai
+# A fun fact about me
+
+print("""
+BBBB  V   V
+B   B V   V
+B   B V   V
+BBBB  V   V
+B   B V   V
+B   B  V V
+BBBB    V
+""")
