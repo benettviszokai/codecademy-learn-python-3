09180289606b16d13cc18660c07d4fb242858fff
Benett Viszokai <benettviszokai@gmail.com>
2025-12-13 19:01:47 +0100

    Create 13-hacking-the-fender.py

---
+++ 13-hacking-the-fender.py
@@ -0,0 +1,51 @@
+"""
+Hacking The Fender
+
+In this project, the task was to practice working with files in Python by
+reading, modifying, and writing data. We accessed and processed CSV and text
+files, obscured sensitive information by scrambling password data, and created
+new output files that included an additional "signature" entry. This exercise
+focused on file I/O, working with CSV data, string manipulation, and generating
+clean output files using Python.
+"""
+
+import csv, json
+
+compromised_users = []
+
+with open('passwords.csv') as password_file:
+  password_csv = csv.DictReader(password_file)
+  for password_row in password_csv:
+    compromised_users.append(password_row['Username'])
+
+with open('compromised_users.txt', 'w') as compromised_user_file:
+  for user in compromised_users:
+    compromised_user_file.write(user)
+
+with open('boss_message.json', 'w') as boss_message:
+  boss_message_dict = {
+    "recipient": "The Boss",
+    "message": "Mission Success"
+  }
+  json.dump(boss_message_dict, boss_message)
+
+with open('new_passwords.csv', 'w') as new_passwords_obj:
+  slash_null_sig = '''
+   _  _     ___   __  ____             
+/ )( \   / __) /  \(_  _)            
+) \/ (  ( (_ \(  O ) )(              
+\____/   \___/ \__/ (__)             
+ _  _   __    ___  __ _  ____  ____  
+/ )( \ / _\  / __)(  / )(  __)(    \ 
+) __ (/    \( (__  )  (  ) _)  ) D ( 
+\_)(_/\_/\_/ \___)(__\_)(____)(____/ 
+        ____  __     __   ____  _  _ 
+ ___   / ___)(  )   / _\ / ___)/ )( \
+(___)  \___ \/ (_/\/    \\___ \) __ (
+       (____/\____/\_/\_/(____/\_)(_/
+ __ _  _  _  __    __                
+(  ( \/ )( \(  )  (  )               
+/    /) \/ (/ (_/\/ (_/\             
+\_)__)\____/\____/\____/
+'''
+  new_passwords_obj.write(slash_null_sig)
